c0c046400b2c85b8126500272336de7cb072c209
J35u5128 <jesusangel.alcalapablo@outlook.com>
2026-04-15 10:23:51 +0200

    Modificacion MainActivity.kt para uso de Snackbar

--- app/src/main/java/com/jalcalap/jetpackcomposesolutions/MainActivity.kt
+++ app/src/main/java/com/jalcalap/jetpackcomposesolutions/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.jalcalap.jetpackcomposesolutions.components.*
 import com.jalcalap.jetpackcomposesolutions.ui.theme.JetpackComposeSolutionsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,12 +31,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeSolutionsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {MyTopAppBar()}) { innerPadding ->
+                val snackbarHostState = remember { SnackbarHostState() }
+                val scope = rememberCoroutineScope()
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {MyTopAppBar()}, snackbarHost = { SnackbarHost(hostState = snackbarHostState) }) { innerPadding ->
                     Box(
-                        modifier = Modifier.fillMaxSize().padding(innerPadding).background(Color.Cyan),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .background(Color.Cyan),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text("")
+                        Text("Esta es mi screen", modifier = Modifier.clickable{
+                            scope.launch{
+                                val result = snackbarHostState.showSnackbar(message = "ejemplo", actionLabel = "Deshacer")
+                                if (result == SnackbarResult.ActionPerformed){
+                                    //deshacer
+                                }else{
+                                    //!deshacer
+                                }
+                            }
+                        })
                     }
                 }
             }
