7bfd78b271a9bf1455c6433001332746ef36604b
J35u5128 <jesusangel.alcalapablo@outlook.com>
2026-04-15 09:57:59 +0200

    Añadido TopAppBar.kt

--- app/src/main/java/com/jalcalap/jetpackcomposesolutions/MainActivity.kt
+++ app/src/main/java/com/jalcalap/jetpackcomposesolutions/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.jalcalap.jetpackcomposesolutions.components.*
 import com.jalcalap.jetpackcomposesolutions.ui.theme.JetpackComposeSolutionsTheme
@@ -20,8 +24,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeSolutionsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyExposedDropDownMenu(Modifier.padding(innerPadding))
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {MyTopAppBar()}) { innerPadding ->
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(innerPadding).background(Color.Cyan),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("")
+                    }
                 }
             }
         }
