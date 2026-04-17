a7bc057763d70b6d00c0e2d15e58d589f6e485c5
J35u5128 <jesusangel.alcalapablo@outlook.com>
2026-04-17 13:22:24 +0200

    Añadida libreria serializable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrainsKotlinSerialization)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
